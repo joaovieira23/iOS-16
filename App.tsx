6aa98c672671124b2f7a2230ad1a92be94b217fd
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-10 11:48:31 -0300

    style: footer ui

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import wallpaper from './assets/images/wallpaper.webp';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -11,6 +11,18 @@ export default function App() {
         <Text style={styles.date}>Segunda-feira, 10 de outubro</Text>
         <Text style={styles.time}>11:10</Text>
       </View>
+
+      {/* Notification List */}
+      <View style={styles.footer}>
+        <View style={styles.icon}>
+          <MaterialCommunityIcons name="flashlight" size={24} color="white" />
+        </View>
+
+        <View style={styles.icon}>
+          <Ionicons name="ios-camera" size={24} color="white" />
+        </View>
+      </View> 
+
       <StatusBar style="auto" />
     </ImageBackground>
   );
@@ -19,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
   },
 
   header: {
@@ -39,5 +51,24 @@ const styles = StyleSheet.create({
     fontSize: 82,
     fontWeight: 'bold',
     color: '#C3FFFE',
+  },
+
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 'auto',
+    alignSelf: 'stretch',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    height: 75
+  },
+
+  icon: {
+    backgroundColor: '#00000050',
+    width: 50,
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
   }
 });
