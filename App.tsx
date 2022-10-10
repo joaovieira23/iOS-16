277bf14b094eeaf6389fd5943b5d62fdfd136a3e
João Andrade <joaovictorvieira.23@hotmail.com>
2022-10-10 11:22:46 -0300

    style: data e time UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import wallpaper from './assets/images/wallpaper.webp';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ImageBackground source={wallpaper} style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons name="ios-lock-closed" size={20} color="white" />
+        <Text style={styles.date}>Segunda-feira, 10 de outubro</Text>
+        <Text style={styles.time}>11:10</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+
+  header: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: 250,
   },
+
+  date: {
+    color: '#C3FFFE',
+    fontSize: 20,
+    fontWeight: '500',
+    marginTop: 20,
+  },
+
+  time: {
+    fontSize: 82,
+    fontWeight: 'bold',
+    color: '#C3FFFE',
+  }
 });
---
+++ assets/data/notifications.js
@@ -0,0 +1,119 @@
+import calendar from "../images/apps/calendar.png";
+import fitness from "../images/apps/fitness.webp";
+import mail from "../images/apps/mail.png";
+
+export default [
+  {
+    id: "1",
+    createdAt: "1m",
+    icon: fitness,
+    title: "Continue assim",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "2",
+    createdAt: "10m",
+    icon: calendar,
+    title: "Reunião de equipe",
+    subtitle: "10:00 - 11:00",
+  },
+  {
+    id: "3",
+    createdAt: "45m",
+    icon: mail,
+    title: "Consulta BrazilJournal",
+    subtitle: "Ei João, como vai? ",
+  },
+  {
+    id: "4",
+    createdAt: "1h",
+    icon: calendar,
+    title: "Team meeting",
+    subtitle: "10:00 - 11:00",
+  },
+  {
+    id: "5",
+    createdAt: "2h",
+    icon: calendar,
+    title: "Reunião de equipe",
+    subtitle: "10:00 - 11:00",
+  },
+  {
+    id: "6",
+    createdAt: "5h",
+    icon: mail,
+    title: "Olá João",
+    subtitle:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
+  },
+  {
+    id: "7",
+    createdAt: "6h",
+    icon: mail,
+    title: "Para a lua",
+    subtitle: "Lorem Ipsum is simply dummy text of the printing  ",
+  },
+  {
+    id: "8",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "9",
+    createdAt: "2h",
+    icon: calendar,
+    title: "Reunião de equipe",
+    subtitle: "10:00 - 11:00",
+  },
+  {
+    id: "10",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "11",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "12",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "13",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "14",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "15",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+  {
+    id: "16",
+    createdAt: "6h",
+    icon: fitness,
+    title: "10.000 degraus",
+    subtitle: "Seu melhor dia esta semana foi sexta-feira. Termine a semana forte.",
+  },
+];
